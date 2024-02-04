7a7a52763feb1b230583fafb8efba1c0ba2412e2
victor_tk <victorkdevelopment@gmail.com>
2024-02-04 21:27:23 +0200

    hero section typography

--- valentines-zeietgeist/src/components/hero.tsx
+++ valentines-zeietgeist/src/components/hero.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Typography } from '@mui/material'
 const Hero = () => {
   return (
     <div id="hero-section">
-    <div id="message">
-      <Typography variant="h3" color="primary.contrastText" >Poems to nourish the soul.</Typography>
-      <Typography variant="h4" color="#CC9B9D" >Poems to nourish the soul.</Typography>
-      <Typography variant="body1" color="#FF5C92"  >Poems tojjj nourish the soul.</Typography>
-      <Typography variant="body1" color="#FF5C92" >Poems to nourish the soul.</Typography>
-      <Typography variant="body1" color="#FF5C92"  >Poems to nourish the soul.</Typography>
-    </div>
-    <div id="hero">hero</div>
+      <div id="message">
+        <div id="typography">
+          <h1 id="header1" className="headings">Poems to nourish the soul.</h1>
+          <h2 id="header2"  className="headings">Poems for the wondering one.</h2>
+          <h3 id="header3"  className="headings">Poems for the yearning heart.</h3>
+          <h4 id="header4" className="headings">Poems for the Zietjeist</h4>
+          <p className="text">Poems to nourish the soul.</p>
+          <p className="text">Poems poems for the yearning heart.</p>
+          <p className="text">Happy valentines.</p>
+        </div>
+      </div>
+      <div id="hero">hero</div>
     </div>
   )
 }
